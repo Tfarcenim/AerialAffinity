e2b84eae185bd69a080dc51440cdf3362386f9af
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-03-03 16:59:17 -0500

    refactoring

--- src/main/java/com/tfar/aerialaffinity/ReferenceVariables.java
+++ src/main/java/com/tfar/aerialaffinity/ReferenceVariables.java
@@ -1,8 +1,8 @@
-package com.tfar.aerialaffinity.util;
+package com.tfar.aerialaffinity;
 
 public class ReferenceVariables {
     public static final String MOD_ID = "aerialaffinity";
     public static final String NAME = "Aerial Affinity";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String MC_VERSION = "1.12.2,";
 }
