b59d5eb27bb759902bc5e08bbfecf99ee39b6e92
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-03-03 16:59:50 -0500

    merge and update to 1.1

--- src/main/java/com/tfar/aerialaffinity/Main.java
+++ src/main/java/com/tfar/aerialaffinity/Main.java
@@ -1,8 +1,21 @@
 package com.tfar.aerialaffinity;
 
-import com.tfar.aerialaffinity.util.ReferenceVariables;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = ReferenceVariables.MOD_ID, name = ReferenceVariables.NAME, version = ReferenceVariables.VERSION)
+@Mod.EventBusSubscriber(modid= ReferenceVariables.MOD_ID)
 public class Main
-{}
+{
+    public static final Enchantment AERIAL_AFFINITY = new EnchantmentAerialAffinity();
+
+    @SubscribeEvent
+    public static void registerEnchantments(RegistryEvent.Register<Enchantment> event) {
+        event.getRegistry().registerAll(AERIAL_AFFINITY);
+    }
+
+
+
+}
