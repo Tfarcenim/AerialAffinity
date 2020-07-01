ece5c0b611d8d0122e6100e6fb5448a0aeb30ce4
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-07-01 11:42:44 -0400

    Update Util.java

--- src/main/java/com/tfar/aerialaffinity/Util.java
+++ src/main/java/com/tfar/aerialaffinity/Util.java
@@ -5,15 +5,13 @@ import baubles.api.cap.IBaublesItemHandler;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
 
+import java.util.stream.IntStream;
+
 import static com.tfar.aerialaffinity.AerialAffinity.AERIAL_AFFINITY;
 
 public class Util {
   public static boolean getAerialAffinity(EntityPlayer player) {
     IBaublesItemHandler handler = BaublesApi.getBaublesHandler(player);
-    for (int i = 0; i < handler.getSlots(); i++) {
-      if (EnchantmentHelper.getEnchantmentLevel(AERIAL_AFFINITY,handler.getStackInSlot(i))>0)
-        return true;
-    }
-    return false;
+    return IntStream.range(0, handler.getSlots()).anyMatch(i -> EnchantmentHelper.getEnchantmentLevel(AERIAL_AFFINITY, handler.getStackInSlot(i)) > 0);
   }
 }
